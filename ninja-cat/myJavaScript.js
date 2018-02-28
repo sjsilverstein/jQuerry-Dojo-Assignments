4690969113eecdb6f54d2aac13f74f5ff018450b
Stephen Silverstein <stephenjsilverstein@gmail.com>
2018-02-27 16:56:04 -0800

    Finished the Debugging Assignment and fixed an error in my ninjacat.js file that saved incorrectly.

--- ninja-cat/myJavaScript.js
+++ ninja-cat/myJavaScript.js
@@ -3,7 +3,8 @@
 
 $(document).ready(function(){
 // BODY
-	$(".main img").click(function(){
+	$(".main img").click(function(){ //On Click do a thing
+
 		var temp= $(this).attr("src");
 		$(this).attr("src", $(this).attr("otherimg"));
 		$(this).attr("otherimg", temp);
